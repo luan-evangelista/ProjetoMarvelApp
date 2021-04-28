7ed4dfed0a706907a4c625fcd588e9505cacc67a
luan-evangelista <luan.evieira43@gmail.com>
2021-04-28 16:22:44 -0300

    Adicionando Projeto: Rotas e Páginas

---
+++ src/pages/Home/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Home() {
+ return (
+   <View>
+       <Text style={{marginTop: 60, fontSize: 25}}>Página Home</Text>
+   </View>
+  );
+}
